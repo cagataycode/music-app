303ebc1902ba8d10e4a08082ade101f68e7d0512
Cagatay Uslu <cagatay.uslu@code.berlin>
2023-04-22 22:34:11 +0200

    create main layout

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,40 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
+const GradientLayout = ({
+  color,
+  children,
+  image,
+  subtitle,
+  title,
+  description,
+  roundImage,
+}) => {
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="40px" align="end">
+        <Box padding="20px">
+          <Image
+            boxSize="250px"
+            boxShadow="2xl"
+            borderRadius={roundImage ? "100%" : "3px"}
+            src={image}
+          />
+        </Box>
+        <Box padding="20px" color="white">
+          <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize="sm">{description}</Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default GradientLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      roundImage
+      color="purple"
+      subtitle="profile"
+      title="Cagatay Uslu"
+      description="22 public playlists"
+      image="https://img.freepik.com/free-photo/children-s-fantasy-tale-with-bison_23-2150165796.jpg?size=626&ext=jpg"
+    >
+      home page
+    </GradientLayout>
+  );
 };
 
 export default Home;
