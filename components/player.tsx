3aa5513d67f931534de1a154dfa2e4dcf176bc94
Cagatay Uslu <cagatay.uslu@code.berlin>
2023-07-11 07:50:24 +0200

    implement player controls state

---
+++ components/player.tsx
@@ -0,0 +1,137 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import ReactHowler from "react-howler";
+import { useEffect, useRef, useState } from "react";
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from "react-icons/md";
+import { useStoreActions } from "easy-peasy";
+
+const Player = ({ songs, activeSong }) => {
+  const [playing, setPlaying] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [seek, setSeek] = useState(0.0);
+  const [repeat, setRepeat] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+  const [duration, setDuration] = useState(0.0);
+
+  const setPlayState = (value) => {
+    setPlaying(value);
+  };
+
+  const onShuffle = () => {
+    setShuffle((state) => !state);
+  };
+
+  const onRepeat = () => {
+    setRepeat((state) => !state);
+  };
+  return (
+    <Box>
+      <Box>
+        {/* <ReactHowler playing={playing} source={activeSong?.url} /> */}
+      </Box>
+      <Center>
+        <ButtonGroup color="gray.600">
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            color={shuffle ? "white" : "gray.600"}
+            icon={<MdShuffle />}
+            onClick={onShuffle}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          {playing ? (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="play"
+              fontSize="48px"
+              color="white"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={() => setPlayState(false)}
+            />
+          ) : (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="pause"
+              fontSize="48px"
+              color="white"
+              icon={<MdOutlinePlayCircleFilled />}
+              onClick={() => setPlayState(true)}
+            />
+          )}
+
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            color={repeat ? "white" : "gray.600"}
+            icon={<MdOutlineRepeat />}
+            onClick={onRepeat}
+          />
+        </ButtonGroup>
+      </Center>
+
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              // eslint-disable-next-line jsx-a11y/aria-proptypes
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              max={300}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">321 </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Player;
