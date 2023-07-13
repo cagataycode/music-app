7fe75ccddcd5becbd0c85e7813e9239eb2d04f61
Cagatay Uslu <cagatay.uslu@code.berlin>
2023-07-13 08:06:56 +0200

    implement music player

--- components/player.tsx
+++ components/player.tsx
@@ -21,14 +21,45 @@ import {
   MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
+import { formatTime } from "../lib/formatters";
 
 const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  );
   const [seek, setSeek] = useState(0.0);
+  const [isSeeking, setIsSeeking] = useState(false);
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
   const [duration, setDuration] = useState(0.0);
+  const soundRef = useRef(null);
+  const repeatRef = useRef(repeat);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
+
+  useEffect(() => {
+    let timerId;
+
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek());
+        timerId = requestAnimationFrame(f);
+      };
+
+      timerId = requestAnimationFrame(f);
+      return () => cancelAnimationFrame(timerId);
+    }
+
+    cancelAnimationFrame(timerId);
+  }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
 
   const setPlayState = (value) => {
     setPlaying(value);
@@ -41,21 +72,68 @@ const Player = ({ songs, activeSong }) => {
   const onRepeat = () => {
     setRepeat((state) => !state);
   };
+
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1;
+    });
+  };
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length);
+
+        if (next === state) {
+          return nextSong();
+        }
+        return next;
+      }
+
+      return state === songs.length - 1 ? 0 : state + 1;
+    });
+  };
+
+  const onEnd = () => {
+    if (repeatRef.current) {
+      setSeek(0);
+      soundRef.current.seek(0);
+    } else {
+      nextSong();
+    }
+  };
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration();
+    setDuration(songDuration);
+  };
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0]));
+    soundRef.current.seek(e[0]);
+  };
+
   return (
     <Box>
       <Box>
-        {/* <ReactHowler playing={playing} source={activeSong?.url} /> */}
+        <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+        />
       </Box>
-      <Center>
-        <ButtonGroup color="gray.600">
+      <Center color="gray.600">
+        <ButtonGroup>
           <IconButton
             outline="none"
             variant="link"
             aria-label="shuffle"
             fontSize="24px"
             color={shuffle ? "white" : "gray.600"}
-            icon={<MdShuffle />}
             onClick={onShuffle}
+            icon={<MdShuffle />}
           />
           <IconButton
             outline="none"
@@ -63,13 +141,14 @@ const Player = ({ songs, activeSong }) => {
             aria-label="skip"
             fontSize="24px"
             icon={<MdSkipPrevious />}
+            onClick={prevSong}
           />
           {playing ? (
             <IconButton
               outline="none"
               variant="link"
-              aria-label="play"
-              fontSize="48px"
+              aria-label="pause"
+              fontSize="40px"
               color="white"
               icon={<MdOutlinePauseCircleFilled />}
               onClick={() => setPlayState(false)}
@@ -78,8 +157,8 @@ const Player = ({ songs, activeSong }) => {
             <IconButton
               outline="none"
               variant="link"
-              aria-label="pause"
-              fontSize="48px"
+              aria-label="play"
+              fontSize="40px"
               color="white"
               icon={<MdOutlinePlayCircleFilled />}
               onClick={() => setPlayState(true)}
@@ -92,6 +171,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="next"
             fontSize="24px"
             icon={<MdSkipNext />}
+            onClick={nextSong}
           />
           <IconButton
             outline="none"
@@ -99,8 +179,8 @@ const Player = ({ songs, activeSong }) => {
             aria-label="repeat"
             fontSize="24px"
             color={repeat ? "white" : "gray.600"}
-            icon={<MdOutlineRepeat />}
             onClick={onRepeat}
+            icon={<MdOutlineRepeat />}
           />
         </ButtonGroup>
       </Center>
@@ -108,16 +188,19 @@ const Player = ({ songs, activeSong }) => {
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
-            <Text fontSize="xs">1:21</Text>
+            <Text fontSize="xs">{formatTime(seek)}</Text>
           </Box>
           <Box width="80%">
             <RangeSlider
-              // eslint-disable-next-line jsx-a11y/aria-proptypes
               aria-label={["min", "max"]}
               step={0.1}
               min={0}
-              max={300}
               id="player-range"
+              max={duration ? (duration.toFixed(2) as unknown as number) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
             >
               <RangeSliderTrack bg="gray.800">
                 <RangeSliderFilledTrack bg="gray.600" />
@@ -126,7 +209,7 @@ const Player = ({ songs, activeSong }) => {
             </RangeSlider>
           </Box>
           <Box width="10%" textAlign="right">
-            <Text fontSize="xs">321 </Text>
+            <Text fontSize="xs">{formatTime(duration)}</Text>
           </Box>
         </Flex>
       </Box>
