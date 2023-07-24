4accb6882aa8bc4cb187f7211a55b379b8e08bb6
Cagatay Uslu <cagatay.uslu@code.berlin>
2023-07-24 17:26:21 +0200

    create playlist when signup

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -44,6 +44,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       secure: process.env.NODE_ENV === "production",
     })
   );
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist ${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 
   res.json(user);
 };
